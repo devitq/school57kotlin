f9e40cb71eb8075071ce551fbcbf760fa269e97b
ITQ <itq.dev@ya.ru>
2025-10-13 19:32:29 +0300

    feat(lesson6): implemented FraudAlertTicketRule

--- lesson6/src/main/kotlin/ru/tbank/education/school/lesson6/creditriskanalyzer/rules/FraudAlertTicketRule.kt
+++ lesson6/src/main/kotlin/ru/tbank/education/school/lesson6/creditriskanalyzer/rules/FraudAlertTicketRule.kt
@@ -1,7 +1,9 @@
 package ru.tbank.education.school.lesson6.creditriskanalyzer.rules
 
 import ru.tbank.education.school.lesson6.creditriskanalyzer.models.Client
+import ru.tbank.education.school.lesson6.creditriskanalyzer.models.PaymentRisk
 import ru.tbank.education.school.lesson6.creditriskanalyzer.models.ScoringResult
+import ru.tbank.education.school.lesson6.creditriskanalyzer.models.TicketTopic
 import ru.tbank.education.school.lesson6.creditriskanalyzer.repositories.TicketRepository
 
 /**
@@ -25,6 +27,27 @@ class FraudAlertTicketRule(
     override val ruleName: String = "Fraud Alert Ticket"
 
     override fun evaluate(client: Client): ScoringResult {
-        TODO()
+        val tickets = ticketRepo.getTickets(client.id)
+
+        var hasFraudAlert = false
+        var unresolvedFraudAlert = false
+
+        for (ticket in tickets) {
+            if (ticket.topic == TicketTopic.FRAUD_ALERT) {
+                hasFraudAlert = true
+                if (!ticket.resolved) {
+                    unresolvedFraudAlert = true
+                    break
+                }
+            }
+        }
+
+        val risk = when {
+            unresolvedFraudAlert -> PaymentRisk.HIGH
+            hasFraudAlert -> PaymentRisk.MEDIUM
+            else -> PaymentRisk.LOW
+        }
+
+        return ScoringResult(ruleName, risk)
     }
 }
