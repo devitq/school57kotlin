a9898f9487c77a44f24cb2e0c12f171ec0991b5b
ITQ <itq.dev@ya.ru>
2025-10-24 17:41:57 +0300

    fix(style): removed detekt check

--- lesson7/build.gradle.kts
+++ lesson7/build.gradle.kts
@@ -1,6 +1,3 @@
-plugins {
-    id("buildlogic.kotlin-library-conventions")
-}
-dependencies {
-    testImplementation(kotlin("test"))
-}
+plugins { buildlogic.`kotlin-common-conventions-no-detekt` }
+
+dependencies { testImplementation(kotlin("test")) }
