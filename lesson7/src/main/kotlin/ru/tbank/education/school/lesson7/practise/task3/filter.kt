ccf4d1cebd3cee62dc97c30aef421269563d5d3f
ITQ <itq.dev@ya.ru>
2025-10-24 17:29:39 +0300

    feat(lesson7): implemented task3

--- lesson7/src/main/kotlin/ru/tbank/education/school/lesson7/practise/task3/filter.kt
+++ lesson7/src/main/kotlin/ru/tbank/education/school/lesson7/practise/task3/filter.kt
@@ -3,8 +3,8 @@ package ru.tbank.education.school.lesson7.practise.task3
 /**
  * Задание 1. Инфиксная фильтрация пользователей
  *
- * Реализуй infix-функции для класса [User], чтобы можно было фильтровать пользователей так,
- * как будто мы пишем на естественном языке.
+ * Реализуй infix-функции для класса [User], чтобы можно было фильтровать пользователей так, как
+ * будто мы пишем на естественном языке.
  *
  * Пример использования:
  * ```
@@ -12,11 +12,10 @@ package ru.tbank.education.school.lesson7.practise.task3
  * val result = users.filter { it livesIn "Москва" }
  * ```
  */
-
 data class User(val name: String, val age: Int, val city: String, val roles: Set<String> = setOf())
 
 // Инфиксная функция для проверки возраста
-infix fun User.ageGreaterThan(age: Int): Boolean = TODO()
+infix fun User.ageGreaterThan(age: Int): Boolean = this.age > age
 
 // Инфиксная функция для проверки города
-infix fun User.livesIn(city: String): Boolean = TODO()
+infix fun User.livesIn(city: String): Boolean = this.city == city
